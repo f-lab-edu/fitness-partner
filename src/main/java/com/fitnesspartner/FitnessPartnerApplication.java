43ca5baebc112be9cb8e4fbded943469b87669f4
jeonga <jeonga>
2023-02-27 22:04:40 +0900

    Feat : 초기셋팅

---
+++ src/main/java/com/fitnesspartner/FitnessPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesspartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessPartnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitnesspartner/FitnessPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnesspartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
