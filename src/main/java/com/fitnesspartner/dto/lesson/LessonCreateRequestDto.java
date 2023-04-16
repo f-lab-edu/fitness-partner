a435f141c751d48a6872383888962d712fdee4cb
naocya@naver.com <naocya@naver.com>
2023-04-16 18:09:06 +0900

    Refactor : Dto Validation 코드 추가

--- src/main/java/com/fitnesspartner/dto/lesson/LessonCreateRequestDto.java
+++ src/main/java/com/fitnesspartner/dto/lesson/LessonCreateRequestDto.java
@@ -2,13 +2,13 @@ package com.fitnesspartner.dto.lesson;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LessonCreateRequestDto {
 
@@ -22,7 +22,8 @@ public class LessonCreateRequestDto {
     @NotBlank(message = "레슨 소개글을 입력해주세요.")
     private String lessonDescription;
 
-    @Size(min = 1, max = 30)
+    @Min(1)
+    @Max(10)
     @NotNull(message = "레슨 정원을 입력해주세요.")
     private int maxEnrollment;
 
@@ -32,9 +33,9 @@ public class LessonCreateRequestDto {
     @NotBlank(message = "센터 주소를 입력해주세요.")
     private String centerAddress;
 
-    @NotBlank(message = "시작 날짜와 시간을 입력해주세요.")
+    @NotNull(message = "시작 날짜와 시간을 입력해주세요.")
     private LocalDateTime startDateTime;
 
-    @NotBlank(message = "종료 날짜와 시간을 입력해주세요.")
+    @NotNull(message = "종료 날짜와 시간을 입력해주세요.")
     private LocalDateTime endDateTime;
 }
